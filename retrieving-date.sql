2524e7100e361e9b9f3c57235d80fe69d9393fa2
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-08-26 21:53:12 +0430

    the where clause

--- retrieving-date.sql
+++ retrieving-date.sql
@@ -1,5 +1,7 @@
 USE sql_store;
 
+SELECT *
+FROM customers;
 
 SELECT *
 FROM customers
@@ -7,4 +9,22 @@ WHERE state IN ('va', 'ma', 'co', 'tx');
 
 
 SELECT state
+FROM customers;
+
+SELECT *
 FROM customers
+WHERE birth_date > '1990-01-01' OR
+      (points > 1000 AND state = 'va');
+
+
+
+SELECT *
+FROM customers
+WHERE NOT (birth_date > '1990-01-01' OR points > 1000);
+
+-- Here is a trick for situations when we have "NOT" operator involved
+-- to negate a statement/condition do the following:
+-- negative of ">" is "<="
+-- negative of "<" is ">="
+-- negative of "=" is "!="
+-- negative of "OR" is "AND" and vice versa
